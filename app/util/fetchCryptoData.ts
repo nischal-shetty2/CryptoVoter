d8f375e92bf3e9a7dca01dd581160a9fe05501b3
Nischal Shetty <nischal2004coc@gmail.com>
2024-07-21 22:00:19 +0530

    Changed solana api

--- app/util/fetchCryptoData.ts
+++ app/util/fetchCryptoData.ts
@@ -4,6 +4,15 @@ import { Model } from "mongoose";
 
 export async function fetchCryptoData(schema: Model<any>, cryptoKey: string) {
   await dbConnect();
+  if (cryptoKey === "solana") {
+    const cryptoData = await schema.find({});
+    return {
+      votes: {
+        bullish: cryptoData[0].bullish,
+        bearish: cryptoData[0].bearish,
+      },
+    };
+  }
   const cryptoData = await schema.find({});
   const priceData = await getPrice();
 
--- app/voting/Solana/page.tsx
+++ app/voting/Solana/page.tsx
@@ -1,15 +1,19 @@
 import { fetchCryptoData } from "@/app/util/fetchCryptoData";
 import CryptoPage from "@/app/components/CryptoPage";
 import { solSchema } from "@/app/models/dbSchema";
+import axios from "axios";
 
 export default async function Solana() {
   try {
-    const data = await fetchCryptoData(solSchema, "solana");
+    const { data } = await axios.get(
+      "https://api.diadata.org/v1/assetQuotation/Solana/0x0000000000000000000000000000000000000000"
+    );
+    const { votes } = await fetchCryptoData(solSchema, "solana");
     return (
       <CryptoPage
         crypto="Solana"
-        price={data.price}
-        votes={data.votes}
+        price={data.Price}
+        votes={votes}
         cryptoKey="sol"
       />
     );
